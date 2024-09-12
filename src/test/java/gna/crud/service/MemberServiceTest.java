8861fc3db8df0a7033070f7479addde9fe2da61a
GinaYu <105768159+stargnastar@users.noreply.github.com>
2024-09-12 17:59:35 +0900

    fix test code

--- src/test/java/gna/crud/service/MemberServiceTest.java
+++ src/test/java/gna/crud/service/MemberServiceTest.java
@@ -30,29 +30,29 @@ class MemberServiceTest {
     @Test
     void 회원생성() {
         // given
-        memberRepository.save(new Member("KIM", "123!@#qwe", "지나", "wlskb@naver.com"));
+        memberRepository.save(new Member("김티맥맥", "123!@#qwe", "티맥이루", "tmax@naver.com"));
 
         // when
         List<Member> members = memberRepository.findAll();
         Member member = members.get(0);
 
         // then
-        Assertions.assertThat(member.getUsername()).isEqualTo("KIM");
+        Assertions.assertThat(member.getUsername()).isEqualTo("김티맥맥");
     }
 
     @Test
     void 회원수정() {
-        memberRepository.save(new Member("KIM", "123!@#qwe", "지나", "wlskbdkfnb@naver.com"));
+        memberRepository.save(new Member("김티맥맥", "123!@#qwe", "티맥이루", "tmax@naver.com"));
         List<Member> members = memberRepository.findAll();
         Member member = members.get(0);
 
-        memberService.update(member.getId(), new MemberEditRequestDto("321!@@@44", "지나아루0813", "wlskbdkfn0813@naver.com"));
+        memberService.update(member.getId(), new MemberEditRequestDto("321!@@@44", "티라미수케잌", "tmax@naver.com"));
 
         Member findMember = memberRepository.findById(member.getId()).get();
 
-        Assertions.assertThat(findMember.getNickname()).isEqualTo("지나아루0813");
+        Assertions.assertThat(findMember.getNickname()).isEqualTo("티라미수케잌");
         Assertions.assertThat(findMember.getPassword()).isEqualTo("321!@@@44");
-        Assertions.assertThat(findMember.getEmail()).isEqualTo("wlskbdkfn0813@naver.com");
+        Assertions.assertThat(findMember.getEmail()).isEqualTo("tmax@naver.com");
 
     }
 }
